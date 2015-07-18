624097d005b4c8df1f82b065e324d65a296f42db
Andy Reischle <andy@reischle.net>
2015-07-18 14:44:49 +0200

    memory friendly DNS server

--- dns-liar.lua
+++ dns-liar.lua
@@ -1,50 +1,29 @@
-svr=net.createServer(net.UDP) 
-svr:on("receive",function(svr,pl)
-decodedns(pl)
-encodedns (query, transaction_id)
-svr:send(response)
+--Thanks to Thomas Shaddack for optimizations - 20150707 ARe
+dns_ip=wifi.ap.getip()
+local i1,i2,i3,i4=dns_ip:match("(%d+)%.(%d+)%.(%d+)%.(%d+)")
+x00=string.char(0)
+x01=string.char(1)
+dns_str1=string.char(128)..x00..x00..x01..x00..x01..x00..x00..x00..x00
+dns_str2=x00..x01..x00..x01..string.char(192)..string.char(12)..x00..x01..x00..x01..x00..x00..string.char(3)..x00..x00..string.char(4)
+dns_strIP=string.char(i1)..string.char(i2)..string.char(i3)..string.char(i4)
+svr=net.createServer(net.UDP)
+svr:on("receive",function(svr,dns_pl)
+  decodedns(dns_pl)
+  svr:send(dns_tr..dns_str1..dns_q..dns_str2..dns_strIP)
+  collectgarbage("collect")
 end)
 svr:listen(53)
-
-function decodedns(pl)
-a=string.len(pl)
-transaction_id  = string.sub(pl, 1, 2)
-bte=""
-query=""
-i=13
-bte2=""
-while bte2 ~= "0" do
-bte = string.byte(pl,i)
-bte2 = string.format("%x", bte )
-query = query .. string.char(bte)
-i=i+1
-end
-end
-
-function encodedns(query, transaction_id)
-local tmpresponse=transaction_id
-tmpresponse=tmpresponse .. unhex "81 80"
-tmpresponse=tmpresponse .. unhex "00 01"
-tmpresponse=tmpresponse .. unhex "00 01"
-tmpresponse=tmpresponse .. unhex "00 00"
-tmpresponse=tmpresponse .. unhex "00 00"
-tmpresponse=tmpresponse .. query
-tmpresponse=tmpresponse .. unhex "00 01"
-tmpresponse=tmpresponse .. unhex "00 01"
-tmpresponse=tmpresponse .. unhex "c0 0c"
-tmpresponse=tmpresponse .. unhex "00 01"
-tmpresponse=tmpresponse .. unhex "00 01"
-tmpresponse=tmpresponse .. unhex "00 00"
-tmpresponse=tmpresponse .. unhex "03 09"
-tmpresponse=tmpresponse .. unhex "00 04"
---IP Address goes here
-tmpresponse=tmpresponse .. unhex "c0 a8" 
-response=tmpresponse .. unhex "04 01" 
-collectgarbage("collect")
-end
-
-function unhex(str)
-    str = string.gsub (str, "(%x%x) ?",
-        function(h) return string.char(tonumber(h,16)) end)
-	    return str
+function decodedns(dns_pl)
+  local a=string.len(dns_pl)
+  dns_tr = string.sub(dns_pl, 1, 2)
+  local bte=""
+  dns_q=""
+  local i=13
+  local bte2=""
+  while bte2 ~= "0" do
+    bte = string.byte(dns_pl,i)
+    bte2 = string.format("%x", bte )
+    dns_q = dns_q .. string.char(bte)
+    i=i+1
+  end
 end
